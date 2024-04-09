4432f7bf200d3ba16d800e9dc71bba279cf9e913
zegervdv <zegervdv@me.com>
2024-04-09 08:13:22 +0200

    feat: add pattern-based incrementing of integers

--- helix-core/src/increment/integer.rs
+++ helix-core/src/increment/integer.rs
@@ -1,4 +1,19 @@
+use regex::Regex;
+
 const SEPARATOR: char = '_';
+
+#[derive(Clone, Copy)]
+enum Base {
+    Base2 = 2,
+    Base8 = 8,
+    Base10 = 10,
+    Base16 = 16,
+}
+
+struct NumberPattern {
+    base: Base,
+    prefix: String,
+}
 
 /// Increment an integer.
 ///
@@ -19,15 +34,37 @@ pub fn increment(selected_text: &str, amount: i64) -> Option<String> {
         return None;
     }
 
-    let radix = if selected_text.starts_with("0x") {
-        16
-    } else if selected_text.starts_with("0o") {
-        8
-    } else if selected_text.starts_with("0b") {
-        2
-    } else {
-        10
+    let patterns = vec![
+        (Base::Base16, Regex::new(r"^(0x|\d*'h)").unwrap()),
+        (Base::Base10, Regex::new(r"^(\d*'d)").unwrap()),
+        (Base::Base8, Regex::new(r"^(0o)").unwrap()),
+        (Base::Base2, Regex::new(r"^(0b|\d*'b)").unwrap()),
+    ];
+
+    let pattern = patterns
+        .iter()
+        .find_map(|(base, matcher)| match matcher.find(selected_text) {
+            Some(m) => Some(NumberPattern {
+                base: base.to_owned(),
+                prefix: m.as_str().to_string(),
+            }),
+            None => None,
+        });
+
+    let pattern = match pattern {
+        Some(p) => p,
+        None => NumberPattern {
+            base: Base::Base10,
+            prefix: "".to_owned(),
+        },
     };
+
+    log::warn!(
+        "base = {:?}; prefix {:?} ({})",
+        pattern.base as u32,
+        pattern.prefix,
+        pattern.prefix.len()
+    );
 
     // Get separator indexes from right to left.
     let separator_rtl_indexes: Vec<usize> = selected_text
@@ -39,47 +76,51 @@ pub fn increment(selected_text: &str, amount: i64) -> Option<String> {
 
     let word: String = selected_text.chars().filter(|&c| c != SEPARATOR).collect();
 
-    let mut new_text = if radix == 10 {
-        let number = &word;
-        let value = i128::from_str_radix(number, radix).ok()?;
-        let new_value = value.saturating_add(amount as i128);
+    let number = &word[pattern.prefix.len()..];
+    log::warn!("Number = {:?}", number);
+    let mut new_text = match pattern.base {
+        Base::Base10 => {
+            let value = i128::from_str_radix(number, pattern.base as u32).ok()?;
+            let new_value = value.saturating_add(amount as i128);
 
-        let format_length = match (value.is_negative(), new_value.is_negative()) {
-            (true, false) => number.len() - 1,
-            (false, true) => number.len() + 1,
-            _ => number.len(),
-        } - separator_rtl_indexes.len();
+            let format_length = match (value.is_negative(), new_value.is_negative()) {
+                (true, false) => number.len() - 1,
+                (false, true) => number.len() + 1,
+                _ => number.len(),
+            } - separator_rtl_indexes.len();
 
-        if number.starts_with('0') || number.starts_with("-0") {
-            format!("{:01$}", new_value, format_length)
-        } else {
-            format!("{}", new_value)
-        }
-    } else {
-        let number = &word[2..];
-        let value = u128::from_str_radix(number, radix).ok()?;
-        let new_value = (value as i128).saturating_add(amount as i128);
-        let new_value = if new_value < 0 { 0 } else { new_value };
-        let format_length = selected_text.len() - 2 - separator_rtl_indexes.len();
-
-        match radix {
-            2 => format!("0b{:01$b}", new_value, format_length),
-            8 => format!("0o{:01$o}", new_value, format_length),
-            16 => {
-                let (lower_count, upper_count): (usize, usize) =
-                    number.chars().fold((0, 0), |(lower, upper), c| {
-                        (
-                            lower + c.is_ascii_lowercase() as usize,
-                            upper + c.is_ascii_uppercase() as usize,
-                        )
-                    });
-                if upper_count > lower_count {
-                    format!("0x{:01$X}", new_value, format_length)
-                } else {
-                    format!("0x{:01$x}", new_value, format_length)
-                }
+            if number.starts_with('0') || number.starts_with("-0") {
+                format!("{:01$}", new_value, format_length)
+            } else {
+                format!("{}{}", pattern.prefix, new_value)
             }
-            _ => unimplemented!("radix not supported: {}", radix),
+        }
+        _ => {
+            let value = u128::from_str_radix(number, pattern.base as u32).ok()?;
+            let new_value = (value as i128).saturating_add(amount as i128);
+            let new_value = if new_value < 0 { 0 } else { new_value };
+            let format_length =
+                selected_text.len() - pattern.prefix.len() - separator_rtl_indexes.len();
+
+            match pattern.base {
+                Base::Base2 => format!("{2}{:01$b}", new_value, format_length, pattern.prefix),
+                Base::Base8 => format!("{2}{:01$o}", new_value, format_length, pattern.prefix),
+                Base::Base16 => {
+                    let (lower_count, upper_count): (usize, usize) =
+                        number.chars().fold((0, 0), |(lower, upper), c| {
+                            (
+                                lower + c.is_ascii_lowercase() as usize,
+                                upper + c.is_ascii_uppercase() as usize,
+                            )
+                        });
+                    if upper_count > lower_count {
+                        format!("{2}{:01$X}", new_value, format_length, pattern.prefix)
+                    } else {
+                        format!("{2}{:01$x}", new_value, format_length, pattern.prefix)
+                    }
+                }
+                _ => unimplemented!("radix not supported: {}", pattern.base as u32),
+            }
         }
     };
 
@@ -100,7 +141,7 @@ pub fn increment(selected_text: &str, amount: i64) -> Option<String> {
             _ => separator_rtl_indexes[0],
         };
 
-        let prefix_length = if radix == 10 { 0 } else { 2 };
+        let prefix_length = pattern.prefix.len();
         if let Some(mut index) = new_text.find(SEPARATOR) {
             while index - prefix_length > spacing {
                 index -= spacing;
@@ -128,6 +169,8 @@ mod test {
             ("-1", 2, "1"),
             ("1", -1, "0"),
             ("1", -2, "-1"),
+            ("32'd100", 1, "32'd101"),
+            ("32'd100", -1, "32'd99"),
         ];
 
         for (original, amount, expected) in tests {
@@ -142,11 +185,24 @@ mod test {
             ("0x0100", -1, "0x00ff"),
             ("0x0001", -1, "0x0000"),
             ("0x0000", -1, "0x0000"),
+            ("16'h0100", 1, "16'h0101"),
+            ("16'h0100", -1, "16'h00ff"),
+            ("16'h0001", -1, "16'h0000"),
+            ("16'h0000", -1, "16'h0000"),
+            ("'h0100", 1, "'h0101"),
+            ("'h0100", -1, "'h00ff"),
+            ("'h0001", -1, "'h0000"),
+            ("'h0000", -1, "'h0000"),
             ("0xffffffffffffffff", 1, "0x10000000000000000"),
             ("0xffffffffffffffff", 2, "0x10000000000000001"),
             ("0xffffffffffffffff", -1, "0xfffffffffffffffe"),
             ("0xABCDEF1234567890", 1, "0xABCDEF1234567891"),
             ("0xabcdef1234567890", 1, "0xabcdef1234567891"),
+            ("64'hffffffffffffffff", 1, "64'h10000000000000000"),
+            ("64'hffffffffffffffff", 2, "64'h10000000000000001"),
+            ("64'hffffffffffffffff", -1, "64'hfffffffffffffffe"),
+            ("64'hABCDEF1234567890", 1, "64'hABCDEF1234567891"),
+            ("64'habcdef1234567890", 1, "64'habcdef1234567891"),
         ];
 
         for (original, amount, expected) in tests {
@@ -201,6 +257,30 @@ mod test {
                 -1,
                 "0b1111111111111111111111111111111111111111111111111111111111111110",
             ),
+            ("128'b00000100", 1, "128'b00000101"),
+            ("128'b00000100", -1, "128'b00000011"),
+            ("128'b00000100", 2, "128'b00000110"),
+            ("128'b00000100", -2, "128'b00000010"),
+            ("128'b00000001", -1, "128'b00000000"),
+            ("128'b00111111", 10, "128'b01001001"),
+            ("128'b11111111", 1, "128'b100000000"),
+            ("128'b10000000", -1, "128'b01111111"),
+            ("128'b0000", -1, "128'b0000"),
+            (
+                "128'b1111111111111111111111111111111111111111111111111111111111111111",
+                1,
+                "128'b10000000000000000000000000000000000000000000000000000000000000000",
+            ),
+            (
+                "128'b1111111111111111111111111111111111111111111111111111111111111111",
+                2,
+                "128'b10000000000000000000000000000000000000000000000000000000000000001",
+            ),
+            (
+                "128'b1111111111111111111111111111111111111111111111111111111111111111",
+                -1,
+                "128'b1111111111111111111111111111111111111111111111111111111111111110",
+            ),
         ];
 
         for (original, amount, expected) in tests {
@@ -217,6 +297,9 @@ mod test {
             ("0x0000_0000_0001", 0x1_ffff_0000, "0x0001_ffff_0001"),
             ("0x0000_0000", -1, "0x0000_0000"),
             ("0x0000_0000_0000", -1, "0x0000_0000_0000"),
+            ("64'h0000_0000_0001", 0x1_ffff_0000, "64'h0001_ffff_0001"),
+            ("64'h0000_0000", -1, "64'h0000_0000"),
+            ("64'h0000_0000_0000", -1, "64'h0000_0000_0000"),
             ("0b01111111_11111111", 1, "0b10000000_00000000"),
             ("0b11111111_11111111", 1, "0b1_00000000_00000000"),
         ];
